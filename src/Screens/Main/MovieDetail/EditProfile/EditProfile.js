1e4e7513333559005b2ecbe025d49f5a1cb8d39b
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-05 02:19:03 +0300

    Edit Profile Created

--- src/Screens/Main/MovieDetail/EditProfile/EditProfile.js
+++ src/Screens/Main/MovieDetail/EditProfile/EditProfile.js
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { SafeAreaView,View, Text } from 'react-native'
 import React from 'react'
 import styles from "./EditProfile.style"
+import Input from "../../../../Components/Input/Input"
+import Button from "../../../../Components/Button/Button"
 const EditProfile = () => {
   return (
-    <View>
-      <Text>EditProfile</Text>
-    </View>
+    <SafeAreaView style={styles.EditProfileContainer}>
+      <View>
+        <Text style={styles.titles}>User Name</Text>
+        <Input placeholder="User Name" />
+        <Text style={styles.titles} >E-Mail</Text>
+        <Input placeholder="E-mail" />
+        <Text style={styles.titles}>Password</Text>
+        <Input placeholder="Password" />
+        <View style={{marginTop:40}}>
+          <Button ButtonName="Save" />
+        </View>
+      </View>
+      <Text></Text>
+    </SafeAreaView>
   )
 }
 
--- src/Screens/Main/MovieDetail/EditProfile/EditProfile.style.js
+++ src/Screens/Main/MovieDetail/EditProfile/EditProfile.style.js
@@ -1,5 +1,14 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    
+    EditProfileContainer : {
+        flex:1,
+        justifyContent:"center",
+        backgroundColor:"#F9F2ED",
+    },
+    titles : {
+        marginLeft:20,
+        fontSize:24,
+        color:"#000",
+    },    
 })
