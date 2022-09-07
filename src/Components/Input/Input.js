32098a56013acb91918c8b8af4b3b05d5f510475
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-07 22:16:22 +0300

    userSlice edited

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,10 +1,10 @@
 import { View, Text,TextInput } from 'react-native'
 import React from 'react'
 import styles from "./Input.style"
-const Input = ({placeholder,onChangeText,value}) => {
+const Input = ({onSubmitEditing,placeholder,onChangeText,value}) => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput placeholderTextColor="#A5C9CA" value={value} onChangeText={onChangeText} style={styles.inputText} placeholder={placeholder} />
+      <TextInput onSubmitEditing={onSubmitEditing} placeholderTextColor="#A5C9CA" value={value} onChangeText={onChangeText} style={styles.inputText} placeholder={placeholder} />
     </View>
   )
 }
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,16 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import axios from "axios";
 export const userSlice = createSlice({
     name :"user",
     initialState : {
-        userInfo : {}
+        userInfo : {
+    
+        }
     },
     reducers : {
-        addUser : (state) => {
+        // setUser : (state,action) => {
+        //     return{
+        //         userInfo: action.payload.userInfo,
+        //     }
+           
+        // },
+   
+        addUser : async (state,action) => {
+             await axios.post('http://10.0.2.2:3000/users',action.payload)
+             //dataya inputtan girilen değerleri gönderiyoruz.
+            return {
+                userInfo:action.payload
+            }
         },
     },
 });
 
-export const {addUser} = userSlice.actions;
+
+export const {setUser,addUser} = userSlice.actions;
 
 export default userSlice.reducer;
