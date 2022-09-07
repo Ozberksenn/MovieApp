e5e01595388d6981d1ba24fe532daac320df77fe
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-07 21:55:22 +0300

    Filtred search page by movie name

--- src/Screens/Main/Search/Search.js
+++ src/Screens/Main/Search/Search.js
@@ -1,14 +1,43 @@
-import { View, Text,TextInput } from 'react-native'
-import React from 'react'
+import { View, Text,TextInput,FlatList, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import styles from "./Search.style"
 import { useSelector } from 'react-redux'
+import MovieCard from '../../../Components/MovieCard/MovieCard'
+import axios from 'axios'
 const Search = () => {
   const {activeTheme} = useSelector(state => state.theme)
+  const [search,setSearch] = useState()
+  const [searchData,setSearchData] = useState(null)
+
+  const handleSubmit = async () => {
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=84c4c1e940f1e77c4ef491c8ff9ede7e&language=en-US&query=${search}&page=1&include_adult=false`)
+    setSearchData(response.data.results)
+  }
+
+
+  // const filtred = value => {
+  //   const filtredList = searchData.filter(item => {
+  //     const searchText = value.toLowerCase();
+  //     const currentTitle = item.title.toLowerCase();
+  //     return currentTitle.indexOf(searchText) > -1;
+  //   })
+  //   setSearch(filtredList)
+  //   setSearchData(search)
+  // }
+  
   return (
     <View style={[styles.container,{backgroundColor:activeTheme.backgroundColor}]}>
       <Text style={[styles.searchTitle,{color:activeTheme.headerTitle}]}>Search</Text>
       <View style={styles.searchInput}>
-        <TextInput placeholderTextColor="#000" style={styles.searchInputText} placeholder='Search'  />
+        <TextInput placeholderTextColor="#000" style={styles.searchInputText} placeholder='Search' 
+        onChangeText={value => setSearch(value)} onSubmitEditing={handleSubmit} />
+      </View>
+      <View>
+        {
+          searchData && (
+            <FlatList data={searchData} renderItem={({item}) => <MovieCard data={item} />}  />
+          )
+        }
       </View>
     </View>
   )
