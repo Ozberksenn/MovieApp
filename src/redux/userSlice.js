4bed5f97b0827512e85dcb5bdfa4b63038c78bae
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-08 19:40:05 +0300

    updateUser slice addded

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import React from "react";
 import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage'; // local storage import.
 export const userSlice = createSlice({
@@ -10,8 +9,7 @@ export const userSlice = createSlice({
     reducers : {
         setUser : (state,action) => {
              AsyncStorage.setItem('userKey',JSON.stringify(action.payload))
-            // state.userInfo = await AsyncStorage.setItem('userKey',JSON.stringify(action.payload))
-            console.log("action Payload : ",action.payload)
+            console.log("SignIn state içerisinde ki değerler: ",action.payload)
             state.userInfo = action.payload
   
         },
@@ -22,10 +20,16 @@ export const userSlice = createSlice({
                 userInfo:action.payload
             }
         },
+        updateUser : (state,action) => {
+            axios.put('http://10.0.2.2:3000/users',action.payload)
+            return {
+                userInfo:action.payload
+            }
+        }
     },
 });
 
 
-export const {setUser,addUser} = userSlice.actions;
+export const {setUser,addUser,updateUser} = userSlice.actions;
 
 export default userSlice.reducer;
