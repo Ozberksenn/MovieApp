8520e591ce181cebd613b6800e7af06d2b710487
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-08 19:39:29 +0300

    edit profile update

--- src/Screens/Main/MovieDetail/EditProfile/EditProfile.js
+++ src/Screens/Main/MovieDetail/EditProfile/EditProfile.js
@@ -1,42 +1,46 @@
-import { SafeAreaView,View, Text } from 'react-native'
+import { SafeAreaView,View, Text, Alert } from 'react-native'
 import React, { useState } from 'react'
 import styles from "./EditProfile.style"
 import Input from "../../../../Components/Input/Input"
 import Button from "../../../../Components/Button/Button"
+import axios from 'axios'
 import { useSelector,useDispatch } from 'react-redux'
-import { addUser } from '../../../../redux/userSlice'
+import { updateUser } from '../../../../redux/userSlice'  
+import { useNavigation } from '@react-navigation/native'
 const EditProfile = () => {
+  const navigation = useNavigation()
   const dispatch = useDispatch();
   const {activeTheme} = useSelector(state => state.theme)
   const {userInfo} = useSelector(state=> state.user)
 
-  // const [userName,setuserName] = useState()
-  // const [mail,setMail] = useState()
-  // const [password,setPassword] = useState()
+  const [mail,setEditMail] = useState()
+  const [password,setEditPassword] = useState()
+  const [passwordAgain,setEditPasswordAgain] = useState()
 
-  // const handleSaveButon = () => {
-  //   postRequests()
-  // }
-
-  // const postRequests = () => {
-  //   dispatch(addUser({email:mail,userName:userName,password:password,passwordAgain:password}))
-  // }
-  // console.log(userInfo)
+  const handleSaveButon = () => {
+    if(mail && password && passwordAgain) {
+      dispatch(updateUser({updateMail:mail,updatePassword:password,updatePasswordAgain:passwordAgain})) // bilgileri global state de değiştirdik.
+      navigation.navigate('MovieDetail')
+    }else{
+      Alert.alert('Warning','values should not be empty')
+    }
+  }
+  console.log("Edit Profil page then edited info, userInfo : ",userInfo)
+ 
 
   return (
     <SafeAreaView style={[styles.EditProfileContainer,{backgroundColor:activeTheme.backgroundColor}]}>
       <View>
-        <Text style={styles.titles}>User Name</Text>
-        <Input onChangeText={(value) => setuserName(value)} placeholder="User Name" />
         <Text style={styles.titles} >E-Mail</Text>
-        <Input defaultValue={userInfo?.email} onChangeText={(value) => setMail(value)} placeholder="E-mail" />
+        <Input defaultValue={userInfo?.signMail} onChangeText={(value) => setEditMail(value)} placeholder="E-mail" />
         <Text style={styles.titles}>Password</Text>
-        <Input onChangeText={(value) => setPassword(value)} placeholder="Password" />
+        <Input defaultValue={userInfo?.signPassword} onChangeText={(value) => setEditPassword(value)} placeholder="Password" />
+        <Text style={styles.titles}>Password Again</Text>
+        <Input defaultValue={userInfo?.signPassword} onChangeText={(value) => setEditPasswordAgain(value)} placeholder="Password Again" />
         <View style={{marginTop:40}}>
-          <Button  ButtonName="Save" />
+          <Button onPress={handleSaveButon} ButtonName="Save" />
         </View>
       </View>
-      <Text></Text>
     </SafeAreaView>
   )
 }
